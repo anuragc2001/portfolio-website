f2eda8bb99d3958a5b13abfb4cc2d2adad714f70
Anurag Chakraborty <chakraborty.anurag01@gmail.com>
2021-11-21 22:48:02 +0530

    added express routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 
 
 const app = express();
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 app.get("/", (req, res) => {
-
+    res.sendFile("/public/index.html")
+})
+app.post("/submit", (req, res) => {
+    res.redirect("/")
+    console.log(JSON.parse(JSON.stringify(req.body)))
 })
 
 app.listen(port, () => {
